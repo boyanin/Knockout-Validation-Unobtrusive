90e3dd9e5778b81fc5cbc0d6221e6cb8130d5fb6
bmihaylov <boyan.mihaylov@ebita.dk>
2013-07-15 21:25:48 +0200

    Add a demo; Update the valitesubmit binding to stop propagation is there is an error

---
+++ demo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KnockoutUnobtrusive.MvcApplication" Language="C#" %>
---
+++ demo/Models/MyModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace KnockoutUnobtrusive.Models
+{
+    public class MyModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Range(10, 90, ErrorMessage = "Enter valid age")]
+        public int Age { get; set; }
+
+        public bool IsEducated { get; set; }
+
+        [RegularExpression(@"\d+", ErrorMessage = "Enter valida phone")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "ExtraName is required")]
+        [Compare("Name", ErrorMessage = "ExtraName must equal Name")]
+        public string ExtraName { get; set; }
+
+        [Required(ErrorMessage = "Must enter a date")]
+        public DateTime Date { get; set; }
+    }
+}
